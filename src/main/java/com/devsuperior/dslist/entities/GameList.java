9e303d794378df64d8eac73eea043ca921525ecf
InfamousRager <infamousragerrr@gmail.com>
2025-08-20 00:09:35 -0300

    Get lists

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -16,33 +16,35 @@ public class GameList {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	
+
 	public GameList() {
 	}
-
 
 	public GameList(Long id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
+	public Long getId() {
+		return id;
+	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	public String getName() {
+		return name;
+	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -55,6 +57,4 @@ public class GameList {
 		GameList other = (GameList) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
